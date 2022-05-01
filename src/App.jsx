874dc335e087dcb18ce993d1993683de1ff6271a
amiriitu <umaturru@gmail.com>
2022-05-02 02:47:23 +0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React from 'react';
+import Main from './Main/Main';
+import Content from './Content/Content';
+import Connect from './Connect/Connect';
+import Footer from './Footer/Footer'
+
+function App() {
+  return (
+    <div className="App-wrapper">
+     <Main />
+     <Content />
+    <Connect />
+    <Footer/>
+     </div>
+  );
+}
+
+export default App;
---
+++ src/Connect/Connect.jsx
@@ -0,0 +1,25 @@
+
+import phoneImg from './img/meetup-phone.png'
+import React from 'react';
+function Connect() {
+  return (
+    <div className="App-wrapper">
+     <div className="connect">
+       <div className="connect-1">
+         <h1>
+         Give your network a boost
+         </h1>
+         <p>
+         Connect with thought leaders, peers, and creators in your industry and beyond.
+         </p>
+         <button>Get Started</button>
+       </div>
+       <div className="connect-2">
+         <img src={phoneImg} alt="" />
+       </div>
+     </div>
+     </div>
+  );
+}
+
+export default Connect;
